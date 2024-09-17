cb2ddf7be57ce8b31d43a1d671d23a650d9f24a0
Alfacodererzho <erzhigitcodingpy@gmail.com>
2024-09-16 21:02:54 -0500

    squirrel color finder

--- day-25/main.py
+++ day-25/main.py
@@ -1,3 +1,20 @@
-import pandas
-data = pandas.read_csv("weather_data.csv")
-print(data)
+import pandas as pd
+
+data = pd.read_csv("2018_Central_park.csv")
+
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+
+df.to_csv("squirrel_count.csv")
