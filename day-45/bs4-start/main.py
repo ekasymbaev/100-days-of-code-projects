24d42609cd241b7380e455a6e97561524e3da103
Alfacodererzho <erzhigitcodingpy@gmail.com>
2024-10-18 17:21:51 -0500

    first commit for day-45

--- day-45/bs4-start/main.py
+++ day-45/bs4-start/main.py
@@ -1,20 +1,70 @@
 from bs4 import BeautifulSoup
-# import lxml (if 'html.parser' does not work)
-with open("website.html") as file:
-    contents = file.read()
+import requests
 
-soup = BeautifulSoup(contents, "html.parser")
+response = requests.get("https://news.ycombinator.com/news")
 
-# print(soup.prettify())
-all_anchor_tags = soup.find_all(name = "a")
+yc_web_page = response.text
 
-# for tag in all_anchor_tags:
-#     print(tag.getText())
-#     print(tag.get("href"))
+soup = BeautifulSoup(yc_web_page, "html.parser")
+span_tags = soup.find_all(name="span", class_="titleline")
 
 
-heading = soup.find(name = "h1", id="name")
-print(heading.getText())
+span_texts = []
+span_links = []
+for span_tag in span_tags:
+    text = span_tag.getText()
+    span_texts.append(text)
+    link = span_tag.find('a')["href"]
+    span_links.append(link)
+# span_upvotes = soup.find_all(name="span", class_='score').getText()
+span_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name = "span", class_="score")]
 
-section_heading = soup.find(name="h3", class_= "heading")
-print(section_heading.getText())
+
+# print(span_texts)
+# print(span_links)
+# print(span_upvotes)
+
+needed_index = span_upvotes.index(max(span_upvotes))
+highest_element = []
+highest_element.append(span_texts[needed_index])
+highest_element.append(span_links[needed_index])
+highest_element.append(span_upvotes[needed_index])
+
+
+print(highest_element)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # import lxml (if 'html.parser' does not work)
+# with open("website.html") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+#
+# # print(soup.prettify())
+# all_anchor_tags = soup.find_all(name = "a")
+#
+# # for tag in all_anchor_tags:
+# #     print(tag.getText())
+# #     print(tag.get("href"))
+#
+#
+# # heading = soup.find(name = "h1", id="name")
+# # print(heading.getText())
+# #
+# # section_heading = soup.find(name="h3", class_= "heading")
+# # print(section_heading.getText())
+#
+# company_url = soup.select_one(selector="p a")
